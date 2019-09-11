7a739291fc61c0c7812ced21eceea3ff2853ff5f
i182807 <valentin.hallet.Etu@univ-lemans.fr>
2019-09-11 16:26:08 +0200

    Valentin Perso

--- ProjetValentin/MainAccueil.php
+++ ProjetValentin/MainAccueil.php
@@ -9,7 +9,7 @@
     <form method="post" action="">
         Pseudo : <input type="text" name="nom" size="10"> <br/><br/>
         Mot de Passe : <input type="text" name="mdp" size="10"><br/><br/>
-        <input type="submit" value="Ajouter" id="submit">
+        <input type="submit" value="Se connecter">
     </form>
 </div>
 </body>
