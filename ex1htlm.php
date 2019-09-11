1013142f1da3795cba68b1dbcb63f0ba7ca6a626
i182807 <valentin.hallet.Etu@univ-lemans.fr>
2019-09-11 15:13:09 +0200

    DébutSérieux

--- ex1htlm.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form method="post" action="ex1.php">
-            Login : <input type="text" name="login" size="10"><br>
-            Password : <input type="text" name="password" size="10"><br>
-            <input type="submit" value="Ajouter">
-        </form>
-    </body>
-</html>
