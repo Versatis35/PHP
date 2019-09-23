f2abdc45f00cd81ac93bae6a12f7fcd5f4d52eea
Gaston Lagaffe <i181329@IUTLAVALAD.UNIV-LEMANS.FR>
2019-09-23 10:13:06 +0200

    charles v2

--- Chales/viewlist.php
+++ Chales/viewlist.php
@@ -1,1 +1,51 @@
-<?php
+
+<html>
+<head>
+   <link type="text/css" rel="stylesheet" href="viewlist.css">
+   <title>TodoMaster</title>
+</head>
+   <body>
+
+
+
+
+   <div class="container">
+       <div class="item header">
+           <div id="titre">
+
+   <!-- TodoMaster-->
+           </div>
+           <div id="logOut">
+               <input type="submit" value="Se déconnecter"><br>
+               <input type="submit" value="Supprimer"><br>
+               <input type="submit" value="modifier">
+
+           </div>
+       </div>
+
+       <div class="item nav">
+           Echeance : <input type="text" name="Echéance" size="50"><br><br><br><br><br><br><br>
+           Note : <input type="text" name="Note" size="50"><br><br><br><br><br><br><br>
+           Lien : <input type="text" name="Lien" size="50"><br>
+       </div>
+<!--
+       <div class="item main">
+           <ul>
+               <li> fbvhgc</li>
+               <li> fbvhgc</li>
+           </ul>
+       </div>
+-->
+       <div class="item footer">
+
+       </div>
+   </div>
+
+   </body>
+</html>
+
+
+
+
+
+
